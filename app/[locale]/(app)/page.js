4486faf436e94d4a4314a0b28206fa265b1e1d9a
Radu Soltan <radusoltan@gmail.com>
2025-04-29 21:00:38 +0300

    new updates 2

--- app/[locale]/(app)/page.js
+++ app/[locale]/(app)/page.js
@@ -35,7 +35,7 @@ export default async function Home({params, searchParams}) {
                       <Link href={`/${locale}/${article.category.slug}/${article.slug}`}>
                           <span
                               className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                              {article.category.title}
+                              {article.category.name}
                           </span>
                       </Link>
                       <span className="text-sm">{
